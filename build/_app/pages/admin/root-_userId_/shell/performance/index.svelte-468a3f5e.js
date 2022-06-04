6ea73ca749f2eb3d614d5928519d8c86d2a5d848
Axel R <regnou.a@gmail.com>
2022-06-04 17:15:07 -0400

    ⬛ --

--- build/_app/pages/admin/root-_userId_/shell/performance/index.svelte-468a3f5e.js
+++
@@ -1,1 +0,0 @@
-import{S as s,i as t,s as a}from"../../../../../chunks/index-81160aa3.js";class l extends s{constructor(e){super();t(this,e,null,null,a,{})}}export{l as default};
--- src/lib/0-shell/ui/category/Category.svelte
+++ src/lib/0-shell/ui/category/Category.svelte
@@ -1,30 +1,30 @@
 <div class="">
-	<!-- <div class="animate-charcter  txt font-varela font-extrabold">{title}</div> -->
+	<!-- <div class="animate-charcter  txt font-quicksand font-extrabold">{title}</div> -->
 	<!--  -->
 	<!-- <div class="txt font-extralight ">{title}</div> -->
-	<!-- <div class="fat font-extrabold  txt font-varela ">{title}</div> -->
-	<!-- <div class="eleguant font-extralight  txt font-varela ">{title}</div> -->
+	<!-- <div class="fat font-extrabold  txt font-quicksand ">{title}</div> -->
+	<!-- <div class="eleguant font-extralight  txt font-quicksand ">{title}</div> -->
 	<div class="letterpress  cat-label--big  font-quicksand font-extrabold sm:text-6xl">
 		{title}
 	</div>
-	<!-- <div class="troisD  txt font-varela font-extrabold">{title}</div> -->
+	<!-- <div class="troisD  txt font-quicksand font-extrabold">{title}</div> -->
 	<!-- <div class="txt font-economica">{title}</div> -->
 </div>
 
 <style>
 	.stylish {
-		@apply font-varela font-extrabold;
+		@apply font-quicksand font-extrabold;
 	}
 	.letterpress {
 		text-shadow: 0px 1px 1px rgba(255, 255, 2555, 0.75), 0px -1px 1px rgba(0, 0, 0, 0.75);
 		/* text-shadow: h-shadow v-shadow blur color;*/
 	}
 	.cat-label--big {
-    @apply text-center text-5xl;
+		@apply text-4xl ;
 		/* @apply py-10 px-10 text-center text-xl uppercase; */
 		/* !! fat */
 		/* !! eleguant */
-		@apply  text-warm-gray-700;
+		@apply text-warm-gray-700;
 		/* !! 3d */
 		/* @apply text-gray-200 bg-gray-500; */
 		/* !! letterpress */
