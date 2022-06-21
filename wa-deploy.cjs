415064e1ecdb183569795532d7b2958b8f44337c
Mickaël MOREAU <mickael.moreau.pro@gmail.com>
2022-06-21 09:28:34 +0200

    wa-config rest deploy API integration in progress

--- wa-deploy.cjs
+++ wa-deploy.cjs
@@ -5,8 +5,10 @@
 * service@monwoo.com
 */
 
-// https://stackabuse.com/making-http-requests-in-node-js-with-node-fetch/
+const fs = require('fs');
+// https://stackabuse.com/making-http-requests-in-node-js-with-node-fetch
 const fetch = require('node-fetch');
+const readline = require("readline");
 
 // https://www.npmjs.com/package/dotenv
 // https://coderrocketfuel.com/article/how-to-load-environment-variables-from-a-.env-file-in-nodejs
@@ -17,6 +19,12 @@ require('dotenv').config();
 const waApiBaseUrl = process.env.WA_REST_API_SERVER
 .replace(/\/+$/g, ''); // .trim('/');
 const waApiUserLocation = process.env.WA_USER_LOCATION;
+
+let [head_target, zip_subpath, zip_bundle] = [
+    process.env.WA_HEAD_TARGET || "wa-axelo-app-cv",
+    process.env.WA_ZIP_SUBPATH || "",
+    process.env.WA_ZIP_ARCHIVEPATH || "build/static.zip"
+];
 
 function assert(test, msg, ...ctx) {
     if (!test) {
@@ -48,18 +56,131 @@ debug("Will deploy from : ", waApiBaseUrl, waApiUserLocation)
 const deployUrl = `${waApiBaseUrl}/fronthead/sync`;
 debug("To url : ", deployUrl);
 
+// https://nodejs.dev/learn/writing-files-with-nodejs
+const secuTmpFile = '.wa-access.tmp';
+let waAccess = {};
+try {
+    if (fs.existsSync(secuTmpFile)) {
+        waAccess = JSON.parse(fs.readFileSync(secuTmpFile, waAccess));
+    }
+} catch (err) {
+    console.error(err);
+}
+
 // const headers = new Headers({
-const headers = {
+const defaultHeaders = {
     accept: 'application/json'
 };
 
-fetch(deployUrl, {
-    method: 'GET',
-    // body: content, method: 'POST'
+const defaultPostData = {
+    sync_action: 'publish',
+    user_location: waApiUserLocation,
+    wa_access_id: waAccess.wa_access_id,
+    wa_api_pre_fetch_token: waAccess.wa_api_pre_fetch_token,
+};
+
+// 🌖🌖 Request : wa-config-by-monwoo deploy api  🌖🌖
+// 🌖🌖           with build at build/static.zip  🌖🌖
+const headers = {
+    ...defaultHeaders,
+    accept: 'application/json'
+};
+debug("With headers data : ", headers);
+
+const postData = {
+    ...defaultPostData,
+    head_target,
+    zip_subpath,
+    zip_bundle,
+}
+debug("With post data : ", postData);
+
+// Try to deploy
+const deploy_init = () => fetch(deployUrl, {
+    method: 'POST',
+    body: postData,
     headers: headers
 })
+// Debug
 .then(res => res.clone().text().then(
     t => debug("Did fetch : ", t)
 ) && res)
+// Json extract
 .then(res => res.json())
-.then(text => console.log(text));
+// Test error and take appropriate actions
+.then(res => {
+    let resp = res;
+    if ('wa_api_login_required' === res.error) {
+        const authLink = res.location;
+        // https://nodejs.org/en/knowledge/command-line/how-to-prompt-for-command-line-input
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+        resp = new Promise((resolve, reject) => {
+            rl.question(`Did you succed to authenticate with :\n${authLink}\n? (y/n)`, userResp => {
+                if ('n' === userResp) {
+                    rl.question(`Deploy will fail, should re-try \n${authLink}\n? (y/n) `, userResp => {
+                        if ('y' === userResp) {
+                            resolve({
+                                ...res,
+                                should_retry: true,
+                            });
+                        } else {
+                            reject(res);
+                        }
+                        rl.close();
+                    });    
+                } else {
+                    rl.close();
+                    resolve({
+                        ...res,
+                        should_retry: true,
+                    });    
+                }
+            });
+        });          
+    }
+    return resp;
+})
+// Debug
+.then(resp => debug("Did load : ", resp) || resp)
+// Retry if should_retry is asked
+.then(resp => resp.should_retry ? deploy_init() : resp)
+// Error handler
+.catch(err => {
+    console.error('Error :', err);
+    return err;
+})
+// Update internals from response
+.then(resp => {
+    wa_api_pre_fetch_token = (resp.wa_api_pre_fetch_token && resp.wa_api_pre_fetch_token.length)
+    ? resp.wa_api_pre_fetch_token : waAccess.wa_api_pre_fetch_token;
+
+    wa_access_id = (resp.wa_access_id && resp.wa_access_id.length)
+    ? resp.wa_access_id : waAccess.wa_access_id;
+
+    waAccess = {
+        ...waAccess,
+        wa_api_pre_fetch_token,
+        wa_access_id
+    }
+
+    return resp;
+});
+
+const deploy = () => deploy_init()
+.then(resp => {
+    console.log(resp);
+
+    // Save headers, cookies, auth, etc...
+    // re-load deploy_init with updated headers will launch it right ;)
+    try {
+        waAccess = fs.writeFileSync(secuTmpFile, JSON.stringify(waAccess));
+    } catch (err) {
+        console.error(err);
+    }    
+})
+
+// Launch the deploy process
+deploy();
